9e0a0e3f5790906f43db505b564a3764d810ff52
DarynaLisova29 <dar.lisova29@gmail.com>
2024-02-18 23:48:35 +0200

    Add class Module,which can create object

--- app/src/main/java/com/example/weather_mvvm_new/repositories/MyRepoModule.java
+++ app/src/main/java/com/example/weather_mvvm_new/repositories/MyRepoModule.java
@@ -1,2 +1,28 @@
-package com.example.weather_mvvm_new.repositories;public class MyRepoModel {
+package com.example.weather_mvvm_new.repositories;
+
+import androidx.lifecycle.LiveData;
+
+import com.example.weather_mvvm_new.model.App;
+import com.example.weather_mvvm_new.model.MyModel;
+import com.example.weather_mvvm_new.model.room.WeatherDao;
+
+import java.util.List;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class MyRepoModule {
+    @Provides
+    public WeatherDao weatherDaoProvides(){
+        return App.getDatabase().getWeatherDao();
+    }
+    @Provides
+    public LiveData<List<MyModel>> allWeatherProvides(WeatherDao weatherDao){
+        return weatherDao.getInfoAllWeather();
+    }
+    @Provides
+    public MyRepo meRepoProvides(WeatherDao weatherDao, LiveData<List<MyModel>>allWeather){
+        return new MyRepo(weatherDao,allWeather);
+    }
 }
