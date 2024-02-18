171e9301436a13d1325d7c040adf5c7c647c8e9c
DarynaLisova29 <dar.lisova29@gmail.com>
2024-02-18 23:47:27 +0200

    Add class Component

--- app/src/main/java/com/example/weather_mvvm_new/repositories/MyRepoComponent.java
+++ app/src/main/java/com/example/weather_mvvm_new/repositories/MyRepoComponent.java
@@ -1,2 +1,10 @@
-package com.example.weather_mvvm_new.repositories;public class MyRepoComponent {
+package com.example.weather_mvvm_new.repositories;
+
+import com.example.weather_mvvm_new.view_model.MyViewModel;
+
+import dagger.Component;
+
+@Component(modules= MyRepoModule.class)
+public interface MyRepoComponent {
+    void inject(MyViewModel myViewModel);
 }
