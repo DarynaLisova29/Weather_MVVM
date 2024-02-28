0dc788a9ae9d9d08e30d63e4eaa585f22ddf1d4b
DarynaLisova29 <dar.lisova29@gmail.com>
2024-02-28 15:40:22 +0200

    some change, add methods

--- app/src/main/java/com/example/weather_mvvm_new/MainActivity.java
+++ app/src/main/java/com/example/weather_mvvm_new/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import com.example.weather_mvvm_new.databinding.ActivityMainBinding;
 import com.example.weather_mvvm_new.model.MyModel;
+import com.example.weather_mvvm_new.model.recycler_view.MyAdapter;
 import com.example.weather_mvvm_new.view_model.MyViewModel;
 
 import java.util.ArrayList;
@@ -23,66 +24,57 @@ import javax.inject.Inject;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    String city;
-
     MyViewModel myViewModel;
-
-    List<MyModel>list=new ArrayList<>();
-//    for RecyclerView
-    RecyclerView recyclerView;
     ActivityMainBinding binding;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
         //viewBinding
-        binding=ActivityMainBinding.inflate(getLayoutInflater());
-        View view=binding.getRoot();
-        setContentView(view);
-
-        myViewModel=new ViewModelProvider(this).get(MyViewModel.class);
-
-
-//       визначаємо recyclerView
-        recyclerView=findViewById(R.id.recycler);
-
-        //вішаємо на кнопку подію
-        binding.getWeather.setOnClickListener(this);
-        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-        myViewModel.getInfo(MainActivity.this,recyclerView);
-
-        myViewModel.getModel().observe(this, new Observer<MyModel>() {
-            @Override
-            public void onChanged(MyModel myModel) {
-                binding.tempValue.setText(String.valueOf(myModel.getMain().getTemp()));
-                binding.condValue.setText(myModel.getWeather().get(0).getMain());
-                binding.pressValue.setText(String.valueOf(myModel.getMain().getPressure()));
-                //метод який відповідає за добавлення
-                myViewModel.insert(myModel);
-                //метод який відповідає за отримання інформації
-                myViewModel.getInfo(MainActivity.this,recyclerView);
-
-            }
-        });
-        myViewModel.errorHandler.observe(this, new Observer<Boolean>() {
-            @Override
-            public void onChanged(Boolean aBoolean) {
-                Toast.makeText(MainActivity.this,
-                                        " Some problems ",Toast.LENGTH_SHORT).show();
-            }
-        });
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initViewModel();
+        setLayoutManager();
+        setListeners();
+        observable();
     }
 
     @Override
     public void onClick(View view) {
         if(view.getId()==R.id.getWeather) {
-//            city = inputCity.getText().toString();
-            city = binding.inputCity.getText().toString();
-            Log.d("MyTag", binding.inputCity.getText().toString() + "");
-            myViewModel.getWeather(city, "metric");
-
-
+            String city = binding.inputCity.getText().toString();
+            myViewModel.getWeather(city, Const.METRIC);
         }
+    }
+
+    private void observable() {
+        myViewModel.getAllWeatherLifeData().observe(this,
+                weathers -> binding.recycler.setAdapter(new MyAdapter(weathers))
+        );
+
+        myViewModel.getModel().observe(this, myModel -> {
+            binding.tempValue.setText(String.valueOf(myModel.getMain().getTemp()));
+            binding.condValue.setText(myModel.getWeather().get(0).getMain());
+            binding.pressValue.setText(String.valueOf(myModel.getMain().getPressure()));
+            //метод який відповідає за добавлення
+            myViewModel.insert(myModel);
+        });
+        myViewModel.errorHandler.observe(this, aBoolean
+                -> Toast.makeText(MainActivity.this,
+                " Some problems ", Toast.LENGTH_SHORT).show()
+        );
+    }
+
+    private void setListeners() {
+        binding.getWeather.setOnClickListener(this);
+    }
+
+    private void setLayoutManager() {
+        //вішаємо на кнопку подію й ресайклView
+        binding.recycler.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+    }
+
+    private void initViewModel() {
+        myViewModel = new ViewModelProvider(this).get(MyViewModel.class);
     }
 }
