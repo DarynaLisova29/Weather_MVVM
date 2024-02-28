b880c459e3684048e119173ddc30e88c71f4b523
DarynaLisova29 <dar.lisova29@gmail.com>
2024-02-28 15:46:02 +0200

    Create module

--- app/src/main/java/com/example/weather_mvvm_new/di/MyRepoModule.java
+++ app/src/main/java/com/example/weather_mvvm_new/di/MyRepoModule.java
@@ -1,2 +1,29 @@
-package com.example.weather_mvvm_new.di;public class MyRepoModule {
+package com.example.weather_mvvm_new.di;
+
+import androidx.lifecycle.LiveData;
+
+import com.example.weather_mvvm_new.model.App;
+import com.example.weather_mvvm_new.model.MyModel;
+import com.example.weather_mvvm_new.model.room.WeatherDao;
+import com.example.weather_mvvm_new.repositories.MyRepo;
+
+import java.util.List;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class MyRepoModule {
+    @Provides
+    public WeatherDao provideWeatherDao(){
+        return App.getDatabase().getWeatherDao();
+    }
+    @Provides
+    public LiveData<List<MyModel>> provideAllWeather(WeatherDao weatherDao){
+        return weatherDao.getInfoAllWeather();
+    }
+    @Provides
+    public MyRepo provideMyRepo(WeatherDao weatherDao, LiveData<List<MyModel>>allWeather){
+        return new MyRepo(weatherDao,allWeather);
+    }
 }
